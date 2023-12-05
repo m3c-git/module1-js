4c93c7762a98ac864bd30db80341ad7e4090b616
m3c-git <ayyouentreprise@gmail.com>
2023-12-05 15:27:08 +0100

    update

---
+++ for/ex-epicerie/partie1/exercice/index.js
@@ -0,0 +1,62 @@
+/**************************************************************/
+/*                          Consigne                          */
+/**************************************************************/
+/* 
+Epicerie
+
+Je suis un épicier, je possède une liste d'articles à vendre.
+Un client vient consulter ma liste d'articles.
+Il peut ou non en acheter un.
+Lorsqu'il décide d'en acheter un, celui-ci disparait de ma liste.
+
+// TODO
+- Un tableau contient la liste d'articles à vendre (dans Data).
+- La liste d'articles est affichée au client.
+- Le programme demande au client quel article il veut acheter
+- Le client choisi un article à acheter :
+  - si l'article est dans la liste : afficher 'ok' et le supprimer de la liste d'articles
+  - sinon afficher 'Article indisponible...'
+
+NOTE : Pour des raisons de simplicité,
+on simulera la suppression d'un article de la liste
+en le remplaçant par une chaine vide '' dans le tableau.
+
+*/
+/**************************************************************/
+/*                            Data                            */
+/**************************************************************/
+
+// DO NOT EDIT
+
+const articles = [
+  'Chips',
+  'Saucisson',
+  'Glace',
+  'Fraises',
+  'Vin',
+  'Sucre',
+  'Bière',
+  'Mouchoirs'
+];
+
+/**************************************************************/
+/*                        Main Program                        */
+/**************************************************************/
+
+// Le code sera implémenté ici...
+
+let choixClient = prompt(`Voici la liste des produits disponible ${articles}. Quesouhaitez-vous achetez ?`);
+
+for(let i = 0; i <= articles.length; i++){
+  if(articles.includes(choixClient)){
+    alert("ok")    
+    articles.splice(articles.indexOf(choixClient), 1, "")
+    console.log(articles);
+    break;
+  }
+  else{
+    alert("article indisponible")
+    continue;
+  }
+
+}
---
+++ for/ex-epicerie/partie2/exercice/index.js
@@ -0,0 +1,35 @@
+/**************************************************************/
+/*                          Consigne                          */
+/**************************************************************/
+/* 
+Epicerie - Variante de la partie 1
+
+On propose au client d'acheter un article tant qu'il y en a de disponibles
+
+// TODO
+Tant qu'il reste des articles différent de '',
+exécuter le programme de la partie 1
+
+*/
+/**************************************************************/
+/*                            Data                            */
+/**************************************************************/
+
+// DO NOT EDIT
+
+const articles = [
+  'Chips',
+  'Saucisson',
+  'Glace',
+  'Fraises',
+  'Vin',
+  'Sucre',
+  'Bière',
+  'Mouchoirs'
+];
+
+/**************************************************************/
+/*                        Main Program                        */
+/**************************************************************/
+
+// Le code sera implémenté ici...
