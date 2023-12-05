6d17a0bc1e433774c8bda5085b30f10c41a8b0a2
m3c-git <ayyouentreprise@gmail.com>
2023-12-05 11:37:42 +0100

    update

--- tableaux/ex-colors/exercice1.js
+++ tableaux/ex-colors/exercice1.js
@@ -2,3 +2,6 @@
  * Déclarer une variable « colors » contenant les valeurs "Rouge" et "Bleu".
  * Afficher "Bleu" dans la console. En utilisant le tableau, hein !
  */
+
+const colors = ["rouge", "bleu"]
+console.log(colors[1])
