94bc66bd793560252f74603f91ddd7bf17e83aa6
m3c-git <ayyouentreprise@gmail.com>
2023-12-06 11:46:01 +0100

    add ex user-management

---
+++ objets/users-management/partie2/exercice/index.js
@@ -0,0 +1,43 @@
+/**************************************************************/
+/*                          Consigne                          */
+/**************************************************************/
+/*
+
+// Voici une collection : cad un tableau d'objets de même type
+// Tu vois la syntaxe ?
+
+let users = [
+  {
+    nom: "Dupont",
+    prenom: "Toto",
+    age: 37
+  }
+];
+
+TODO
+- Demander à l'utilisateur de saisir son prenom, nom, age
+NOTE : penser à gérer les types de valeurs
+- Puis créer un objet (comme celui déjà présent) avec ces 3 éléments
+- Enfin, l'ajouter à la collection
+- Pour finir, afficher le contenu de la collection
+
+*/
+/**************************************************************/
+/*                            Data                            */
+/**************************************************************/
+
+// DO NOT EDIT
+
+let users = [
+  {
+    nom: "Dupont",
+    prenom: "Toto",
+    age: 37
+  }
+];
+
+/**************************************************************/
+/*                        Main Program                        */
+/**************************************************************/
+
+// Le code sera implémenté ici...
